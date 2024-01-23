3400b5d7458cc20dbe64458d168d8fcd9c54fa69
Venâncio <joaovitorvenancio99@outlook.com>
2024-01-23 17:02:53 +0000

    Sound Manager added

--- Assets/Devs/Venancio/Scripts/SoundManager.cs
+++ Assets/Devs/Venancio/Scripts/SoundManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
@@ -10,9 +11,16 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     private AudioClipDictionaryPage[] m_backgroundMusicsData;
     public float BackgroundVolume = 1f;
-    
+    private Dictionary<string, AudioClip> backgroundMusics = new Dictionary<string, AudioClip>();
 
 
+    private void Awake()
+    {
+        foreach(AudioClipDictionaryPage backgroundMusic in m_backgroundMusicsData)
+        {
+            backgroundMusics.Add(backgroundMusic.Name, backgroundMusic.AudioClip);
+        }
+    }
 
     [Serializable]
     public struct AudioClipDictionaryPage
