14c4497b314656e55fb8d8921fc9093fad555af9
Venâncio <joaovitorvenancio99@outlook.com>
2024-01-24 23:31:23 +0000

    Audio Manager can tag audio sources and individually change the groups volume

--- Assets/Devs/Venancio/Scripts/AudioManager.cs
+++ Assets/Devs/Venancio/Scripts/AudioManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+//TO-DO: No audioGroupList, fazer uma lista de um objeto proprio, onde se possa colocar um float e uma string).
 public class AudioManager : MonoBehaviour
 {
     [Header("Background Musics Configuration")]
@@ -54,14 +54,29 @@ public class AudioManager : MonoBehaviour
     private List<AudioSource> _activeAudioSourceList = new List<AudioSource>();
     private Dictionary<AudioSource, float> _orignalVolumeDictionary = new Dictionary<AudioSource, float>();
 
+    [Space][Space][Space]
+
+    [Header("Custom Audio Groups")]
     [Space]
-    [Space]
-    [Space]
+
+    [SerializeField]
+    private List<string> _audioGroupList = new List<string>();
+    private Dictionary<string, List<AudioSource>> _audioGroupDictionary = new Dictionary<string, List<AudioSource>>();
+
+
+    [Space][Space][Space]
 
     [Header("General Configuration")]
     [Space]
 
     public AudioSource BackgroundMusicAudioSource;
+
+    //[Serializable]
+    //public struct AudioGroupVolumeDictionary
+    //{
+    //    public string AudioGroupName;
+    //    public float Volume;
+    //}
 
     [Serializable]
     public struct AudioClipDictionaryPage
@@ -107,6 +122,8 @@ public class AudioManager : MonoBehaviour
 
         if (_playOnStart)
             PlayBackgroundMusic(_backgroundMusicToPlay);
+
+        
     }
 
     private void CheckVariables()
@@ -116,7 +133,16 @@ public class AudioManager : MonoBehaviour
             BackgroundMusicAudioSource = gameObject.AddComponent<AudioSource>();
         }
 
-        
+        if (_audioGroupList != null)
+        {
+            foreach (string audioGroup in _audioGroupList)
+            {
+                _audioGroupDictionary.Add(audioGroup, new List<AudioSource>());
+            }
+        } else
+        {
+            _audioGroupList = new List<string>();
+        }
     }
 
     public void PlayBackgroundMusic(string backgroundMusicName)
@@ -190,4 +216,104 @@ public class AudioManager : MonoBehaviour
         targetAudioSource.Play();
     }
 
+    private List<AudioSource> RetrieveAudioSourceListByAudioGroup(string audioGroupName, string methodThatCalled)
+    {
+        List<AudioSource> audioGroupList = null;
+
+        try
+        {
+            audioGroupList = _audioGroupDictionary[audioGroupName];
+        }
+        catch (KeyNotFoundException exception)
+        {
+            audioGroupList = new List<AudioSource>();
+            _audioGroupDictionary.Add(audioGroupName, audioGroupList);
+            _audioGroupList.Add(audioGroupName);
+
+            Debug.Log("AudioManager on " + methodThatCalled + ": no Audio Group with name [" + audioGroupName + "]. Creating a new one...");
+        }
+
+        return audioGroupList;
+    }
+
+    public void Play(string audioClipName, GameObject targetObjectToPlayClip, string audioGroupName)
+    {
+        //Debug.Log("BAGAS: " + GlobalSoundEffectsVolume);
+        AudioClip audioClipToPlay = null;
+
+        try
+        {
+            audioClipToPlay = _audioClips[audioClipName];
+        }
+        catch (KeyNotFoundException exception)
+        {
+            Debug.Log("AudioManager on Play: no AudioClip with name [" + audioClipName + "]. Exception: " + exception.Message);
+            return;
+        }
+
+        AudioSource targetAudioSource = targetObjectToPlayClip.GetComponent<AudioSource>();
+
+        List<AudioSource> audioGroupList = RetrieveAudioSourceListByAudioGroup(audioGroupName, "Play");
+
+        if (targetAudioSource != null)
+        {
+            if (!_activeAudioSourceList.Contains(targetAudioSource))
+            {
+                _orignalVolumeDictionary.Add(targetAudioSource, targetAudioSource.volume);
+                targetAudioSource.volume = targetAudioSource.volume * GlobalSoundEffectsVolume;
+                _activeAudioSourceList.Add(targetAudioSource);
+                audioGroupList.Add(targetAudioSource);
+            } else if (!audioGroupList.Contains(targetAudioSource))
+            {
+                audioGroupList.Add(targetAudioSource);
+            }
+
+        }
+        else
+        {
+            targetAudioSource = targetObjectToPlayClip.AddComponent<AudioSource>();
+            _orignalVolumeDictionary.Add(targetAudioSource, targetAudioSource.volume);
+            targetAudioSource.volume = targetAudioSource.volume * GlobalSoundEffectsVolume;
+            _activeAudioSourceList.Add(targetAudioSource);
+            audioGroupList.Add(targetAudioSource);
+        }
+
+        //Debug.Log(_orignalVolumeDictionary[targetAudioSource].ToString());
+
+        targetAudioSource.clip = audioClipToPlay;
+        targetAudioSource.Play();
+    }
+
+    public void ChangeVolume (float volume, string audioGroup)
+    {
+        List<AudioSource> audioGroupAudioSourceList = RetrieveAudioSourceListByAudioGroup(audioGroup, "ChangeVolume");
+
+        if (audioGroupAudioSourceList.Count > 0)
+        {
+            foreach (AudioSource audioSource in audioGroupAudioSourceList)
+            {
+                //Debug.Log("UMA");
+                audioSource.volume = _orignalVolumeDictionary[audioSource] * volume;
+            }
+        }
+    }
+
+    private int i = 0;
+
+    public void DebugThis(string args)
+    {
+        Debug.Log(_audioGroupDictionary[args].Count);
+        if (i > 0)
+        {
+            Debug.Log("AQ");
+            ChangeVolume(0.5f, args);
+            AudioSource audioSource = _audioGroupDictionary[args][0];
+            Debug.Log(audioSource.volume);
+            Debug.Log(GlobalSoundEffectsVolume);
+
+        } else
+        {
+            i++;
+        }
+    }
 }
--- Assets/Devs/Venancio/Scripts/Testee.cs
+++ Assets/Devs/Venancio/Scripts/Testee.cs
@@ -9,7 +9,11 @@ public class Testee : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioManager.Play("Gaga", gameObject);
+        audioManager.Play("Gaga", gameObject, "Teste");
+        audioManager.ChangeVolume(1f, "Teste");
+        //audioManager.DebugThis("Teste");
+
+        //audioManager.GlobalSoundEffectsVolume = 0f;
 
         //audioManager.Play("Gaga", gameObject);
 
